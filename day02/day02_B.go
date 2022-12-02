769ba4678bbb15fb3d3e3b307b9c8c981c3d740b
BahadirAydin <bahadiraydin1923@gmail.com>
2022-12-02 14:10:50 +0300

    converted indentation to spaces

--- day02/day02_B.go
+++ day02/day02_B.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+  "bufio"
+  "fmt"
+  "os"
 )
 
 func game() int {
-	f, _ := os.Open("input.txt")
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	point := 0
-	for scanner.Scan() {
-		text := scanner.Text()
-		fst := text[0]
-		snd := text[2]
-		if fst == 'A' {
-			if snd == 'X' {
-				point += 3
-			} else if snd == 'Y' {
-				point += 4
-			} else if snd == 'Z' {
-				point += 8
-			}
-		} else if fst == 'B' {
-			if snd == 'X' {
-				point += 1
-			} else if snd == 'Y' {
-				point += 5
-			} else if snd == 'Z' {
-				point += 9
-			}
-		} else if fst == 'C' {
-			if snd == 'X' {
-				point += 2
-			} else if snd == 'Y' {
-				point += 6
-			} else if snd == 'Z' {
-				point += 7
-			}
-		}
-	}
-	return point
+  f, _ := os.Open("input.txt")
+  defer f.Close()
+  scanner := bufio.NewScanner(f)
+  point := 0
+  for scanner.Scan() {
+    text := scanner.Text()
+    fst := text[0]
+    snd := text[2]
+    if fst == 'A' {
+      if snd == 'X' {
+        point += 3
+      } else if snd == 'Y' {
+        point += 4
+      } else if snd == 'Z' {
+        point += 8
+      }
+    } else if fst == 'B' {
+      if snd == 'X' {
+        point += 1
+      } else if snd == 'Y' {
+        point += 5
+      } else if snd == 'Z' {
+        point += 9
+      }
+    } else if fst == 'C' {
+      if snd == 'X' {
+        point += 2
+      } else if snd == 'Y' {
+        point += 6
+      } else if snd == 'Z' {
+        point += 7
+      }
+    }
+  }
+  return point
 }
 func main() {
-	fmt.Println(game())
+  fmt.Println(game())
 }
