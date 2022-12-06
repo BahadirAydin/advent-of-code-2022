1ec38e40e6d03deff30da77428299cb0b91cdd11
BahadirAydin <bahadiraydin1923@gmail.com>
2022-12-06 08:58:50 +0300

    Added Day 06

---
+++ day06/day06_A.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+  "bufio"
+  "fmt"
+  "os"
+)
+
+func countDiff() int {
+  f, _ := os.Open("input.txt")
+  defer f.Close()
+  scanner := bufio.NewScanner(f)
+  scanner.Scan()
+  text := scanner.Text()
+  const n = 4
+  var arr [n - 1]int32
+  diff := 0
+Loop:
+  for k, v := range text {
+    for i := 0; i < diff; i++ {
+      if arr[i] == v {
+        arr[0] = v
+        diff = 1
+        continue Loop
+      }
+    }
+    if diff == n-1 {
+      return k
+    }
+    arr[diff] = v
+    diff++
+  }
+  return 0
+}
+func main() {
+  fmt.Println(countDiff())
+}
