f9834a2b9d2daaa2432c1b2a884d8e76d04df05d
BahadirAydin <bahadiraydin1923@gmail.com>
2022-12-10 11:47:09 +0300

    Added Day 10: It was fun.

--- day10/day10_A.go
+++ day10/day10_A.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
+  "bufio"
+  "fmt"
+  "os"
+  "strconv"
 )
 
 func signalPower(clock, reg int) int {
-	if clock == 20 || clock == 60 || clock == 100 || clock == 140 || clock == 180 || clock == 220 {
-		return clock * reg
-	}
-	return 0
+  if clock == 20 || clock == 60 || clock == 100 || clock == 140 || clock == 180 || clock == 220 {
+    return clock * reg
+  }
+  return 0
 }
 
 func simulate() (sum int) {
-	f, _ := os.Open("input.txt")
-	defer f.Close()
-	reg := 1
-	clock := 0
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		clock++
-		sum += signalPower(clock, reg)
-		str := scanner.Text()
-		if str[0] == 'a' { //addx
-			clock++
-			sum += signalPower(clock, reg)
-			n, _ := strconv.Atoi(str[5:])
-			reg += n
-		}
-	}
-	return
+  f, _ := os.Open("input.txt")
+  defer f.Close()
+  reg := 1
+  clock := 0
+  scanner := bufio.NewScanner(f)
+  for scanner.Scan() {
+    clock++
+    sum += signalPower(clock, reg)
+    str := scanner.Text()
+    if str[0] == 'a' { //addx
+      clock++
+      sum += signalPower(clock, reg)
+      n, _ := strconv.Atoi(str[5:])
+      reg += n
+    }
+  }
+  return
 }
 
 func main() {
-	fmt.Println(simulate())
+  fmt.Println(simulate())
 }
--- day10/day10_B.go
+++ day10/day10_B.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
+  "bufio"
+  "fmt"
+  "os"
+  "strconv"
 )
 
 func draw(clock, reg int) {
 
-	if reg == clock%40 || reg-1 == clock%40 || reg+1 == clock%40 {
-		fmt.Print("#")
-	} else {
-		fmt.Print(".")
-	}
+  if reg == clock%40 || reg-1 == clock%40 || reg+1 == clock%40 {
+    fmt.Print("#")
+  } else {
+    fmt.Print(".")
+  }
 
-	if clock%40 == 0 {
-		fmt.Println()
-	}
+  if clock%40 == 0 {
+    fmt.Println()
+  }
 }
 func simulate() {
-	f, _ := os.Open("input.txt")
-	defer f.Close()
-	reg := 1
-	clock := 0
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		clock++
-		draw(clock, reg)
-		str := scanner.Text()
-		if str[0] == 'a' { //addx
-			clock++
-			n, _ := strconv.Atoi(str[5:])
-			reg += n
-			draw(clock, reg)
-		}
-	}
+  f, _ := os.Open("input.txt")
+  defer f.Close()
+  reg := 1
+  clock := 0
+  scanner := bufio.NewScanner(f)
+  for scanner.Scan() {
+    clock++
+    draw(clock, reg)
+    str := scanner.Text()
+    if str[0] == 'a' { //addx
+      clock++
+      n, _ := strconv.Atoi(str[5:])
+      reg += n
+      draw(clock, reg)
+    }
+  }
 }
 
 func main() {
-	simulate()
+  simulate()
 }
